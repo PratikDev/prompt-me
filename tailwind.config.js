1245c8bb33be5d906b8b9959950e6d5d31740e74
pratik dev <pratikdevofficial1@gmail.com>
2023-08-07 02:20:31 +0600

    linting modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./icons/**/*.{ts,tsx}",
   ],
   theme: {
     container: {
