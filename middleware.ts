6562db0d42bb3c0d9326ecc760e07fe9e83ad7ed
pratik dev <pratikdevofficial1@gmail.com>
2023-08-30 22:00:03 +0600

    temporary UNSAFE protected route

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+const NOT_LOGGED_IN_ROUTES = ["/login"];
+const LOGGED_IN_ROUTES = ["/edit-profile"];
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const notLoggedInRoutes = ["/login"];
-  const loggedInRoutes = ["/edit-profile"];
-
   const currentPath = request.nextUrl.pathname;
 
   const AppwriteProject = process.env.APPWRITE_PROJECT_ID ?? "";
@@ -20,13 +20,30 @@ export async function middleware(request: NextRequest) {
   const isUserLoggedIn =
     cookieStore.has(sessionNames[0]) || cookieStore.has(sessionNames[1]);
 
-  // If the user is logged in and tries to access a `notLoggedInRoutes` route, redirect to the homepage
-  if (isUserLoggedIn && notLoggedInRoutes.includes(currentPath))
-    return NextResponse.redirect(new URL("/", request.url));
+  /*
+    TODO: 
+    Use below method to implement middleware validation.
+    but before that, need to research with the `isomorphic-form-data` issue
+  */
+  // const { AppwriteServices } = await import("@/AppwriteServices");
+  // const appwriteServices = new AppwriteServices();
+  // const isUserLoggedIn = (await appwriteServices.getCurrentUser()) !== null;
 
-  // If the user is not logged in and tries to access a `loggedInRoutes` route, redirect to the login page
-  if (!isUserLoggedIn && loggedInRoutes.includes(currentPath))
+  /*
+    If the user is logged in and tries to access any
+    `notLoggedInRoutes` route, redirect to the homepage
+  */
+  if (isUserLoggedIn && NOT_LOGGED_IN_ROUTES.includes(currentPath)) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  /*
+    If the user is not logged in and tries to access any
+    `loggedInRoutes` route, redirect to the login page
+  */
+  if (!isUserLoggedIn && LOGGED_IN_ROUTES.includes(currentPath)) {
     return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
 
 // See "Matching Paths" below to learn more
