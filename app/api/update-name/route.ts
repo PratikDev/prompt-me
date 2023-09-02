e6bd5277b899512774baec7e8284f0ac2dae70b8
pratik dev <pratikdevofficial1@gmail.com>
2023-09-02 16:24:12 +0600

    update username feature added

--- app/api/update-name/route.ts
+++ app/api/update-name/route.ts
@@ -1,8 +1,31 @@
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
+  const { username } = await request.json();
+
+  const { usernameSchema } = await import("@/schema/schema");
+  const schemaResponse = usernameSchema.safeParse(username);
+
+  if (!schemaResponse.success) {
+    const message = schemaResponse.error.issues[0].message;
+    return NextResponse.json({ success: false, message }, { status: 400 });
+  }
+
+  try {
+    // Update the username in the database
+    const { account } = await import("@/AppwriteServices");
+    await account.updateName(schemaResponse.data);
+  } catch (error: any) {
+    console.log(error);
+
+    return NextResponse.json(
+      { success: false, message: error.message },
+      { status: 500 }
+    );
+  }
+
   return NextResponse.json(
-    { success: false, message: "Username update failed" },
-    { status: 500 }
+    { success: true, message: "Username updated successfully" },
+    { status: 200 }
   );
 }
