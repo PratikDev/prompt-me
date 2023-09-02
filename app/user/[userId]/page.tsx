6f0a14d4652240a7ecb0a3a6cb25756c06e9b5d0
pratik dev <pratikdevofficial1@gmail.com>
2023-09-02 16:36:51 +0600

    added back button in profile pages

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -1,10 +1,15 @@
 import ContentSection from "@/components/ContentSection";
+import BackButton from "@/components/helpers/BackButton";
 import { FC } from "react";
 
 const page: FC<{ params: { userId: string } }> = ({ params: { userId } }) => {
   return (
     <>
-      <ContentSection createdBy={userId} />
+      <BackButton />
+
+      <div className="pt-16 pl-10">
+        <ContentSection createdBy={userId} />
+      </div>
     </>
   );
 };
